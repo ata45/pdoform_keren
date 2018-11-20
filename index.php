1c95d1f24f959cba63c5c3856988edd6fcffef68
ata45 <34927815+ata45@users.noreply.github.com>
2018-11-20 21:36:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,45 +14,22 @@
 </head>
 <body>
   <nav class="navbar navbar-default">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="https://www.malasngoding.com">Malas Ngoding</a>
-		</div>
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="https://www.malasngoding.com">Malas Ngoding</a>
+			</div>
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="https://www.malasngoding.com">Home <span class="sr-only">(current)</span></a></li>
+				<li class="active"><a href="https://www.malasngoding.com">Home</a></li>
 				<li><a href="#">Profil</a></li>
 				<li><a href="#">Tentang Kami</a></li>
 				<li><a href="#">Kontak</a></li>
-				<li class="dropdown">
-				<a class="dropdown-toggle" data-toggle="dropdown" href="#">Tutorial
-					<span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">HTML</a></li>
-						<li><a href="#">CSS</a></li>
-						<li><a href="#">Bootstrap</a></li>
-					</ul>
-				</li>
 			</ul>
-			<form class="navbar-form navbar-left" role="search">
-				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Search">
-				</div>
-				<button type="submit" class="btn btn-default">Submit</button>
-			</form>
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
+			<li><a href="#"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
 				<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
 			</ul>
-		</div><!-- /.navbar-collapse -->
-	</div>
-</nav>
+		</div>
+	</nav>
     <div class="container">
         <h3 class="mt-3 mb-3">Form Input Biodata</h3>
         <form action="store.php" method="post">
